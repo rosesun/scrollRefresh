7e8686ae017876dc76586920336a78f8763951d2
huangfuyan <huangfuyan@zbj.com>
2017-09-14 11:34:08 +0800

    添加加载提示

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="scroll-box">
         <div class="scroll"
-        @touchstart="touchStart($event)"
-        @touchmove="touchMove($event)"
         @touchend="touchEnd($event)">
               <section class="inner">
                   <header class="pull-refresh">
                       <slot name="pull-refresh">
                           <span class="down-tip">下拉更新</span>
                           <span class="up-tip">松开刷新数据</span>
-                          <span class="refresh-tip">加载中……</span>
+                          <span class="refresh-tip" v-show="downLoading">加载中……</span>
                       </slot>
                   </header>
-                  <slot>
+                  <slot
                       <ul>
                           <li v-for="item in dataRefresh">
                               {{ item.name }}
@@ -24,8 +22,8 @@
                   </slot>
                   <footer class="load-more">
                       <slot name="load-more">
-                          <span >上啦加载更多</span>
-                          <span>加载中……</span>
+                          <span >上拉加载更多</span>
+                          <span v-show="upLoading">加载中……</span>
                       </slot>
                   </footer>
                   <div class="nullData" >暂无更多数据</div>
@@ -39,6 +37,8 @@ import axios from 'axios'
 export default {
   data() {
       return{
+          upLoading:false,
+          downLoading:false,
           clientHeight:0,//当前屏幕的高度
           scrollTop:0,//滚出页面顶部距离
           innerHeight:0,//页面高度
@@ -48,31 +48,26 @@ export default {
 
 },
 methods:{
-    touchStart(e){
-        console.log(this.$el.scrollTop);
-
-    },
-    touchMove(e){
-        console.log(this.$el.scrollTop);
-
-    },
     touchEnd(e){
+        var $vm = this;
         this.clientHeight = this.$el.clientHeight;
         this.scrollTop = this.$el.scrollTop;
         this.innerHeight = this.$el.scrollHeight;
-        console.log(123);
-        console.log(this.$el.scrollTop + 'dd1');//滚出页面的高度
-        console.log(this.$el.clientHeight + 'dd2');//当前屏幕的高度
-        console.log(this.$el.scrollHeight + 'dd3');//当前页面的高度
+        console.log(this.$el.scrollTop);//滚出页面的高度
+        console.log(this.$el.clientHeight);//当前屏幕的高度
+        console.log(this.$el.scrollHeight);//当前页面的高度
 
         if( this.scrollTop + this.clientHeight + 5 >= this.innerHeight){
             console.log('到达底部');
-            this.getList();
+            $vm.upLoading = true;
+            $vm.getList();
+
         }else{
             console.log('未到达底部')
         }
         if (this.scrollTop == 0){
             console.log('到达顶部');
+            this.downLoading = true;
             this.refresh();
 
         }
@@ -82,13 +77,15 @@ methods:{
         axios.get('/api/moreData').then((res) => {
             if(res.data.success){
                 this.dataList = this.dataList.concat(res.data.dataList);
-
             } else {
                 console.log(res)
             }
+            this.upLoading = false;
 
         }).catch( (error) => {
-            console.log(error)
+            console.log(error);
+            this.upLoading = false;
+
         });
     },
     refresh(){
@@ -98,8 +95,10 @@ methods:{
             } else {
                 console.log(res);
             }
+            this.downLoading = false;
         }).catch( (error) => {
             console.log(error);
+            this.downLoading = false;
         })
     }
 
@@ -117,7 +116,7 @@ html,body{
     overflow: scroll;
 }
 .scroll{
-    height:2000px;
+    height:1000px;
 }
 
 </style>
